98dcb46e0b6332b99f8e075da8dfa21d652036bd
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-09 18:08:04 +0000

    Update: '0-minoperations.py' converted to int

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,7 +9,15 @@ def minOperations(n, ops=0):
     '''
     number of operations required to get n Hs
     '''
+#    ops = 0
+#    clipboard = 2
+#    while n > 1:
+#        while n % clipboard == 0:
+#            ops += clipboard
+#            n = n / clipboard
+#        clipboard += 1
+#    return ops
     for i in range(2, (n // 2) + 1)[::-1]:
         if n % i == 0:
             return minOperations(i, ops + (n / i))
-    return ops + n
+    return int(ops + n)
