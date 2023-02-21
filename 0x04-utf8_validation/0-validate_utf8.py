d23c8333da72a26d4a8310fc83dfb238a0a35eb1
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-21 18:44:54 +0000

    Update: '0-validate_utf8.py' modified

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,6 +9,31 @@ def validUTF8(data):
     verify utf-8 compatibility
     '''
     for i in data:
-        if i >= 128:
+        binary_i = decimalToBinary(i)
+        no_byte = charLength(binary_i)
+        if not no_byte or no_byte > 4 or len(str(binary_i)) > 8:
             return False
     return True
+
+
+def decimalToBinary(n):
+    '''
+    Converts a decimal to binary
+    '''
+    return bin(n).replace("0b", "")
+
+
+def charLength(b):
+    '''
+    Calculates number of bytes of character
+    '''
+    count = 0
+    binary_digits = str(b)
+    if binary_digits[0] == '0':
+        return 1
+    for char in binary_digits:
+        if char == '1':
+            count += 1
+        elif char == '0':
+            return count
+    return None
